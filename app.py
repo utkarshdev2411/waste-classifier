714aa8841553358b6a026aba4938ddc42ab5b01f
Priyanshu Kumar <priyanshukr19@gmail.com>
2024-05-18 10:43:31 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+model = tf.keras.models.load_model('my_keras_model.h5')
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def preprocess_image(image_path, target_size=(224, 224)):
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=target_size)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    return img_array
+
+def predict_single_image(image_path, model):
+    preprocessed_img = preprocess_image(image_path)
+    prediction = model.predict(preprocessed_img)
+    predicted_class = np.argmax(prediction[0])
+    return predicted_class
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        predicted_class_index = predict_single_image(file_path, model)
+        class_names = ['battery', 'biological', 'cardboard', 'clothes', 'glass', 'metal', 'paper', 'plastic', 'shoes']
+        predicted_class_name = class_names[predicted_class_index]
+        return jsonify({'predicted_class': predicted_class_name})
+
+if __name__ == '__main__':
+    app.run(debug=True)
